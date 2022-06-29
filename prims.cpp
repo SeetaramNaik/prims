cf9bfc801b1ae1fc31b995e969046dccb21853eb
Seetaram Naik <70059359+SeetaramNaik@users.noreply.github.com>
2022-06-30 00:58:24 +0530

    Update prims.cpp

--- prims.cpp
+++ prims.cpp
@@ -3,6 +3,11 @@
 
 #include "stdafx.h"
 
+// prims.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
 #include<stdlib.h>
 #include<iostream>
 #include<glut.h>
@@ -20,7 +25,7 @@ int costmatrix[100][100],undo_cost_stack[100],lineundostack[100][100];
 int undone=0,nf=0,f=0,found=0,nodenum=1,load=0;
 int t[100][2],totalcost;
 bool paused=false;
-bool enterpressed=false,resdisplay=false;
+bool start=false,enterpressed=false,resdisplay=false,show_algorithm=false;
 char undostack[100];
 char num[2];
 char text[15]="Draw mode:NONE";
@@ -88,42 +93,74 @@ void bitmap_output(int x, int y, char *string, void *font)
 
 //this delays out execution of next instruction
 void delay(){
-int i=190000000;
+//int i=190000000;
+	int i=190000;
 while(i--);
 }
 
+
+
 //this function displays staring window
-void frontpage()
+void introduction_page()
 {
+glBegin(GL_POLYGON);
+glColor3f(0.9,0.3,0.1);
+glVertex2d(0,0);
+glColor3f(0.0,0.0,0.0);
+glVertex2d(0,height);
+glColor3f(0.0,0.0,0.0);
+glVertex2d(width,height);
+glColor3f(1.0,0.4,0.0);
+glVertex2d(width,0);
+glEnd();
+	 
+	
 	glLineWidth(2);
-	glColor3f(0.9,0.8,0.9);
-	bitmap_output(width/3+50, height-height/10, "WELCOME! TO",GLUT_BITMAP_TIMES_ROMAN_24);
-	glColor3f(0.9,0.8,0.0);
-	glBegin(GL_LINE_LOOP); 
-		glVertex2f(width/3+30, height-height/9);
-		glVertex2f(width/3+240, height-height/9);
-	glEnd();
-	glColor3f(0.9,0.8,0.9);
-	bitmap_output(width/3+10, height-height/6, "Prims algorithm visualiser",GLUT_BITMAP_TIMES_ROMAN_24);
-	glColor3f(0.9,0.8,0.0);
+	glColor3f(1.0,1.0,1.0);
+	
+	bitmap_output(width/3, height-height/10, "SDM Institute of Technology, Ujire",GLUT_BITMAP_HELVETICA_18);
+	//glColor3f(0.9,0.8,0.0);
+
+	glColor3f(1.0,1.0,1.0);
+	bitmap_output(width/4+50, height-height/6, "Computer Graphics Lab with Mini Project",GLUT_BITMAP_HELVETICA_18);
+	glColor3f(1.0,1.0,1.0);
+	bitmap_output(width/4+55, height-height/4, "****PRIMS ALGORITHM****",GLUT_BITMAP_TIMES_ROMAN_24);
+	/*glColor3f(0.9,0.8,0.0);
 	glBegin(GL_LINE_LOOP); 
 		glVertex2f(width/3-10, height-height/5.5);
 		glVertex2f(width/3+290, height-height/5.5);
-	glEnd();
-	glColor3f(0.0,1.0,0.0);
-	bitmap_output(width/3+100,height-height/3.5,"Made by",GLUT_BITMAP_TIMES_ROMAN_24);
-	bitmap_output(width/3+10,height-height/2.8,"Seetaram Naik - 4SU19CS088",GLUT_BITMAP_TIMES_ROMAN_24);
+	glEnd();*/
+	glColor3f(1.0,1.0,1.0);
+	bitmap_output(width/15+55,height-height/1.87,"---Made by---",GLUT_BITMAP_HELVETICA_18);
+
+	glColor3f(0.8,1.0,0.0);
+	bitmap_output(width/15+10,height-height/1.62,"Seetaram Naik - 4SU19CS088",GLUT_BITMAP_HELVETICA_18);
+	bitmap_output(width/15+10,height-height/1.5,"Shamanth P     - 4SU19CS089",GLUT_BITMAP_HELVETICA_18);
+	bitmap_output(width/15+10,height-height/1.4,"Shivam Ravan  - 4SU19CS091",GLUT_BITMAP_HELVETICA_18);
+	bitmap_output(width/15+10,height-height/1.3,"Skanda Bhat    - 4SU19CS095",GLUT_BITMAP_HELVETICA_18);
+
+	glColor3f(1.0,1.0,1.0);
+	bitmap_output(width/15+580,height-height/1.6,"---Guided by---",GLUT_BITMAP_HELVETICA_18);
+	glColor3f(0.8,1.0,0.0);
+	bitmap_output(width/15+595,height-height/1.4,"Mr. Arjun K.",GLUT_BITMAP_TIMES_ROMAN_24);
+	glFlush();//glutSwapBuffers(); 
+	delay();
+	glColor3f(1.0,1.0,0.9);
+	bitmap_output(width/5+150, 30, "Press 'ENTER' to continue",GLUT_BITMAP_TIMES_ROMAN_24);
 	glFlush();//glutSwapBuffers();
 	delay();
-	glColor3f(1,0.1,0.2);
-	bitmap_output(width/2+150, 125, "Press Enter to Continue.......",GLUT_BITMAP_HELVETICA_18);
-	glFlush();//glutSwapBuffers();
-	delay();
-	glutPostRedisplay();
+	//glutPostRedisplay();
 }
 
+void instruction_page()
+{
+	glColor3f(1.0,1.0,0.0);
+	bitmap_output(10, height-height/10, "Instructions",GLUT_BITMAP_TIMES_ROMAN_24);
+}
+
+
 //this function displays text instructions in grpahics windwow
-void Instructions()
+void draw_instructions()
 {
 	glColor3f(1.0,1.0,0.0);
 	bitmap_output(10, height-height/10, "Instructions",GLUT_BITMAP_TIMES_ROMAN_24);
@@ -138,6 +175,37 @@ void Instructions()
 	bitmap_output(10,height-height/5.8, "->Press E to draw Edges",GLUT_BITMAP_HELVETICA_18);
 	bitmap_output(10, height-height/4.9, "->Press D to draw spanning tree",GLUT_BITMAP_HELVETICA_18);
 }
+
+
+void algorithm()
+{ 
+glBegin(GL_POLYGON);
+//glColor3f(1.0,0.4,0.0);
+glColor3f(0.9,0.3,0.1);
+glVertex2d(0,0);
+///glColor3f(0.0,0.0,0.0);
+glColor3f(0.0,0.138,0.96);
+glVertex2d(0,height);
+glColor3f(0.0,0.138,0.96);
+glVertex2d(width,height);
+glColor3f(1.0,0.4,0.0);
+glVertex2d(width,0);
+glEnd();
+glColor3f(1.0,1.0,1.0);
+bitmap_output(width/6+25, height-50, "***PRIMS ALGORITHM WORKING STEPS***",GLUT_BITMAP_TIMES_ROMAN_24);
+bitmap_output(width/15+25, height-150, "1.  Create a mstSet that keeps track of vertices already included in MST. ",GLUT_BITMAP_HELVETICA_18);
+bitmap_output(width/15+25, height-190, "2.  Initialize all key values as INFINITE. Assign the key value as 0 for the first vertex so",GLUT_BITMAP_HELVETICA_18);
+bitmap_output(width/15+45, height-230, "  that it is picked first.",GLUT_BITMAP_HELVETICA_18);
+bitmap_output(width/15+25, height-270, "3.  While mstSet doesn't include all vertices ",GLUT_BITMAP_HELVETICA_18);
+bitmap_output(width/15+45, height-310, "...a)  Pick a vertex u which is not there in mstSet and has a minimum key value. ",GLUT_BITMAP_HELVETICA_18);
+bitmap_output(width/15+45, height-350, "...b)  Include u to mstSet. ",GLUT_BITMAP_HELVETICA_18);
+bitmap_output(width/15+45, height-390, "...c)  Update key value of all adjacent vertices of u.  ",GLUT_BITMAP_HELVETICA_18);
+bitmap_output(width/15+82, height-410, " To update the key values, iterate through all adjacent vertices. ",GLUT_BITMAP_HELVETICA_18); 
+bitmap_output(width/15+82, height-450, " For every adjacent vertex v, if the weight of edge u-v is less than the previous ",GLUT_BITMAP_HELVETICA_18);
+bitmap_output(width/15+82, height-490, " key value of v, update the key value as the weight of u-v. ",GLUT_BITMAP_HELVETICA_18);
+}
+
+
 //this function draws edges using location stored in linex array
 void drawline(){
 	glColor3f(0.9f,1.0f,0.8f);
@@ -187,7 +255,7 @@ void blinking_lines(){
 		bitmap_output((oldx[t[i][0]][0]+oldx[t[i][1]][0])/2-15,(oldx[t[i][0]][1]+oldx[t[i][1]][1])/2+8,cst,GLUT_BITMAP_TIMES_ROMAN_24);
 	}*/
 	glFlush();//glutSwapBuffers();
-	}
+	}   
 }
 
 //this displays a point when mouse clicked for first time in draw edge mode
@@ -202,6 +270,21 @@ void drawPointAt(float x,float y){
 
 //this function mimics loading animation
 void loadpage(){
+	
+	glBegin(GL_POLYGON);
+//glColor3f(1.0,0.4,0.0);
+//glColor3f(0.9,0.3,0.1);//orange
+	glColor3f(0.5,0.4,0.9);
+glVertex2d(0,0);
+glColor3f(0.0,0.0,0.0);
+glVertex2d(0,height);
+glColor3f(0.0,0.0,0.0);
+glVertex2d(width,height);
+glColor3f(0.5,0.4,0.9);
+glVertex2d(width,0);
+glEnd();
+glColor3f(1.0,1.0,1.0);
+	bitmap_output(100,325,"LOADING",GLUT_BITMAP_HELVETICA_18);
 	delay();
 	glLineWidth(20);
 	glColor3f(0.0,1.0,0.0);
@@ -252,21 +335,27 @@ void display(){
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 	if(load<800 && found==0){
-			glColor3f(1,1,1.0);
-			bitmap_output(100,325,"LOADING",GLUT_BITMAP_HELVETICA_18);
+			//glColor3f(1.0,1.0,1.0);
+			//bitmap_output(100,325,"LOADING",GLUT_BITMAP_HELVETICA_18);
 		    loadpage();
 		}
 	else if(load<800 && found==1){
 	        glColor3f(1,1,1.0);
 			bitmap_output(100,325,"CALCULATING",GLUT_BITMAP_HELVETICA_18);
 		    loadpage();
-	}else{
+	}
+	
+	else{
 	if(!enterpressed){
+		if(show_algorithm){
+			algorithm();
+		}else{
 		glLineWidth(25);
-		frontpage();
+		introduction_page();
+		}
 	}else if(enterpressed){
 			if(!found){
-				Instructions();
+				draw_instructions();
 				glColor3f(0.1,1,0.0);
 				bitmap_output(width-width/2+200,height-height/9,text,GLUT_BITMAP_HELVETICA_18);
 	 		}
@@ -495,6 +584,10 @@ void keyboardfun(unsigned char key,int x,int y){
 		 case 'd':found=0;
 			      find();
 			      break;
+		 case 'i':
+		 case 'I':
+			      show_algorithm=true;
+			      break;
 		 case ' ':paused=!paused;
 			      break;
 		 case 27 :exit(0);
@@ -612,4 +705,5 @@ int main(int argc,char** argv){
 	glutMainLoop();
 	return 0;
 }
+
 
